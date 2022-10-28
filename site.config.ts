08fe42fd8e07780433a2f128f74e239feb39a4ef
i1mT <i@iimt.me>
2022-10-28 21:40:07 +0800

    feat: add page width

--- site.config.ts
+++ site.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: i1mT
+ * @Date: 2022-10-28 10:15:43
+ * @LastEditors: i1mT
+ * @LastEditTime: 2022-10-28 21:32:54
+ * @Description:
+ * @FilePath: \nextjs-notion-starter-kit\site.config.ts
+ */
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
@@ -49,16 +57,12 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '61fdd8cbd1a74d2790d61804fe02930b'
+    }
+  ]
 })
